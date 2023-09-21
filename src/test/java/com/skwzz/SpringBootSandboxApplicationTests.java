1996e88b1b381abb82df557975fe75b2c1a7d655
skwzz <kidol9108@gmail.com>
2023-09-21 23:40:50 +0900

    feat: 최초 커밋 및 샘플 컨트롤러 생성

---
+++ src/test/java/com/skwzz/SpringBootSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skwzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
