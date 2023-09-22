3c6a7964e07564067c6ddedd6a45f2e8b3e29f28
skwzz <kidol9108@gmail.com>
2023-09-23 01:01:56 +0900

    style: 미사용 import 제거

--- src/main/java/com/skwzz/domain/post/dto/ReadPostDto.java
+++ src/main/java/com/skwzz/domain/post/dto/ReadPostDto.java
@@ -3,9 +3,6 @@ package com.skwzz.domain.post.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.web.bind.annotation.GetMapping;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
