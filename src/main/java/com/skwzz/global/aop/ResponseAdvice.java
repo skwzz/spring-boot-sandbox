9829989f23a224f38a90d68437bcb965ea174184
skwzz <kidol9108@gmail.com>
2023-09-24 21:50:23 +0900

    feat: AOP 사용한 공통 response 기초 코드 적용

---
+++ src/main/java/com/skwzz/global/aop/ResponseAdvice.java
@@ -0,0 +1,31 @@
+package com.skwzz.global.aop;
+
+import com.skwzz.global.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ResponseAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        log.info("RESPONSE ADVICE");
+        log.info(body.toString());
+        return ApiResponse.<Object>builder()
+                .result(true)
+                .body(body)
+                .build();
+    }
+}
