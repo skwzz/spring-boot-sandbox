36306acc9abee6c9f5b4458151b3b629006d1599
skwzz <kidol9108@gmail.com>
2023-09-24 19:15:31 +0900

    feat: 공통 Response 에러처리부분 NULL값일 때 포함 안되도록 설정

--- src/main/java/com/skwzz/global/response/ApiResponse.java
+++ src/main/java/com/skwzz/global/response/ApiResponse.java
@@ -1,5 +1,8 @@
 package com.skwzz.global.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Getter
@@ -9,6 +12,8 @@ import lombok.*;
 public class ApiResponse<T> {
     private boolean result;
     private T body;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String errorCode;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String errorMessage;
 }
